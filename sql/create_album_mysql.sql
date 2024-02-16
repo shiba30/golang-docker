37e351c2d305c2f029738dcba06d69d19e17bf2e
shiba30 <shina.17923@gmail.com>
2024-02-16 16:39:09 +0900

    AWSデプロイ用にpostgresに変更

--- sql/create_album_mysql.sql
+++ sql/create_album_mysql.sql
@@ -1,8 +1,8 @@
 DROP TABLE IF EXISTS album;
 CREATE TABLE album (
-  id         BIGSERIAL NOT NULL,
+  id         INT AUTO_INCREMENT NOT NULL,
   title      VARCHAR(128) NOT NULL,
   artist     VARCHAR(255) NOT NULL,
   price      DECIMAL(5,2) NOT NULL,
-  PRIMARY KEY (id)
+  PRIMARY KEY (`id`)
 );
